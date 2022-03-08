77890b13d40d2543cb5f87e149eeb64e07b979f7
Waltlxj <liw4@carleton.edu>
2022-03-07 22:37:32 -0600

    coloring

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,15 +1,41 @@
 import React from 'react'
 import './Intro.css'
+import { SwitchTransition, CSSTransition } from "react-transition-group";
+
+
+const hobbies = ['coding', 'nature', 'people-watching', 'sci-fi', 'photography & digital art', 'classical singing']
+//const colors = ['#4f8104', '#f3b700', '#80a141', '#9d8e00']
 
 const Intro = () => {
+    const [index, setIndex] = React.useState(0)
+    const changeHobby = () => {
+        setIndex(index+1)
+        console.log(index)
+    }
+    setTimeout(changeHobby, 2500)
+
     React.useEffect(() => {
-        window.scroll({ top: 0, behavior: 'smooth' });
+        window.scroll({ top: 0, behavior: 'smooth' })
       }, []);
+
     return (
         <div className='docs-section'>
             <h6 className='docs-header'>About me</h6>
             <p> I am a current college junior majoring in Psychology and CS at Carleton College, MN. </p>
-            <p>I have many hobbies.</p>
+            <p> I like 
+            <SwitchTransition mode='out-in'>
+            <CSSTransition
+                key={index}
+                addEndListener={(node, done) => {
+                node.addEventListener("transitionend", done, false);
+                }}
+                classNames="fade-inline"
+            >
+                <span> {hobbies[index%hobbies.length]}.</span>
+            </CSSTransition>
+            </SwitchTransition>
+            </p>
+                
             <div className='icon-list'>
                 <a className='icon' href="https://www.linkedin.com/in/xiongju-walt-li-a12922173/" target="blank"><i className="fa-brands fa-linkedin fa-xl"></i></a>
 			    <a className='icon' href="https://github.com/Waltlxj" target="blank"><i className='fa-brands fa-github fa-xl'></i></a>
